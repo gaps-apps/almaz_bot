1fe6db488e840a5a552a6065df150e5abe7228d1
fuzz88 <ivan@oschepkov.ru>
2025-03-24 17:39:35 +0800

    refactor config

--- config/__init__.py
+++ config/__init__.py
@@ -1,24 +1,3 @@
-import os
-from logger import logfire
-from typing import TypedDict
-
-
-class Config(TypedDict):
-    LF_TOKEN: str
-    BOT_TOKEN: str
-    WEBHOOK_BASE: str
-    LOMBARDIS_USER: str
-    LOMBARDIS_PASSWORD: str
-
-
-def get_from_env() -> Config:
-    try:
-        return {
-            var: os.environ[var] for var in Config.__annotations__
-        }  # Ensures values are non-None
-    except KeyError as e:
-        logfire.error(f"Missing env var: {e}")
-        exit(1)
-
+from .load import get_from_env
 
 conf = get_from_env()
---
+++ config/load.py
@@ -0,0 +1,24 @@
+import os
+from logger import logfire
+from typing import TypedDict
+
+
+class Config(TypedDict):
+    ADMIN_SECRET: str
+    LF_TOKEN: str
+    BOT_TOKEN: str
+    WEBHOOK_BASE: str
+    WEB_SERVER_HOST: str
+    WEB_SERVER_PORT: str
+    LOMBARDIS_USER: str
+    LOMBARDIS_PASSWORD: str
+
+
+def get_from_env() -> Config:
+    try:
+        return {
+            var: os.environ[var] for var in Config.__annotations__
+        }  # Ensures values are non-None
+    except KeyError as e:
+        logfire.error(f"Missing env var: {e}")
+        exit(1)
