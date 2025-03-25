fbb11d4f8fa4a9e69d1fa66a7dd1ccb2ec0367b3
fuzz88 <ivan@oschepkov.ru>
2025-03-25 22:58:00 +0800

    polling instead of webhooks

--- main.py
+++ main.py
@@ -1,16 +1,13 @@
 import asyncio
 
-from lombardis.api import LombardisAPI
-from repository.clients import fetch_and_store_clients
+from repository.clients import fetch_and_update_local_db
 from repository.users import create_users_table
-from telegram.bot import serve_webhook
 
-from config import conf
+from telegram.bot import get_dispatcher
 
 if __name__ == "__main__":
     asyncio.run(create_users_table())
+    asyncio.run(fetch_and_update_local_db())
 
-    lombardis = LombardisAPI(conf["LOMBARDIS_USER"], conf["LOMBARDIS_PASSWORD"])
-    asyncio.run(fetch_and_store_clients(api=lombardis))
-
-    serve_webhook()
+    dp, bot = get_dispatcher()
+    asyncio.run(dp.start_polling(bot))
--- telegram/bot.py
+++ telegram/bot.py
@@ -1,60 +1,21 @@
-import hashlib
-import random
-
-from aiohttp import web
-
 from aiogram import Bot, Dispatcher, Router
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
-from aiogram.webhook.aiohttp_server import SimpleRequestHandler, setup_application
 
 from config import conf
-from logger import logfire
 
 from .handlers import setup_handlers
 
 TOKEN = conf["BOT_TOKEN"]
 
-WEBHOOK_PATH = "/webhook"
-WEBHOOK_SECRET = hashlib.sha256(random.randbytes(128)).hexdigest()
-BASE_WEBHOOK_URL = conf["WEBHOOK_BASE"]
 
-
-async def on_startup(bot: Bot) -> None:
-    await bot.set_webhook(
-        f"{BASE_WEBHOOK_URL}{WEBHOOK_PATH}", secret_token=WEBHOOK_SECRET
-    )
-    logfire.info(
-        f'Webhook URL: "{BASE_WEBHOOK_URL}{WEBHOOK_PATH}" Secret: "{WEBHOOK_SECRET}"'
-    )
-
-
-async def on_shutdown(bot: Bot) -> None:
-    await bot.delete_webhook()
-    logfire.info("Webhook deleted.")
-
-
-def serve_webhook() -> None:
+def get_dispatcher() -> tuple[Dispatcher, Bot]:
     dp = Dispatcher()
     router = Router()
 
     setup_handlers(router)
     dp.include_router(router)
 
-    dp.startup.register(on_startup)
-    dp.shutdown.register(on_shutdown)
-
     bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
 
-    app = web.Application()
-
-    webhook_requests_handler = SimpleRequestHandler(
-        dispatcher=dp,
-        bot=bot,
-        secret_token=WEBHOOK_SECRET,
-    )
-    webhook_requests_handler.register(app, path=WEBHOOK_PATH)
-
-    setup_application(app, dp, bot=bot)
-
-    web.run_app(app, host=conf["WEB_SERVER_HOST"], port=int(conf["WEB_SERVER_PORT"]))
+    return dp, bot
