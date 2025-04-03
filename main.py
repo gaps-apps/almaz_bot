42790e5d2bad5ac22484a80e2bba960d1b56ec2d
fuzz88 <ivan@oschepkov.ru>
2025-04-03 17:03:14 +0800

    in-memory users stub

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from aiohttp import web
 from config import conf
 
 from lombardis.api import LombardisAPI, LombardisAPIProtocol
-from repository.users import UsersRepo
+from repository.users import UsersRepo, UsersRepoProtocol
 
 from telegram.bot import get_dispatcher
 from telegram.webhook import get_webhook_app
@@ -14,10 +14,8 @@ from telegram.handlers.commands_menu import set_bot_commands
 
 from tests.fakes.lombardis import LombardisAPIFake
 
-users = UsersRepo()
 
-
-async def init(bot: Bot) -> None:
+async def init(bot: Bot, users: UsersRepoProtocol) -> None:
     await asyncio.gather(
         users.bootstrap(),
         set_bot_commands(bot),
@@ -26,14 +24,19 @@ async def init(bot: Bot) -> None:
 
 if __name__ == "__main__":
     dp, bot = get_dispatcher()
-
     loop = asyncio.new_event_loop()
-    loop.run_until_complete(init(bot))
 
-    lombardis: LombardisAPIProtocol = LombardisAPI()
+    lombardis: LombardisAPIProtocol
+    users: UsersRepoProtocol
 
     if conf["DEMO_MODE"].lower() == "true":
         lombardis = LombardisAPIFake()
+        users = UsersRepo(db_name=":memory:")
+    else:
+        lombardis = LombardisAPI()
+        users = UsersRepo()
+
+    loop.run_until_complete(init(bot, users))
 
     if conf["POLLING"].lower() == "true":
         # polling mode
